1aba2efe95612e34d49f8f5f4fc501ce57763e4f
Raphael Leal Carletti <raphael.carletti@monitoratec.com.br>
2018-06-28 14:05:05 -0300

    Alamofire pod installed

---
+++ AlamofireTraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamofireTraining
+//
+//  Created by Raphael Carletti on 6/28/18.
+//  Copyright © 2018 Raphael Carletti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-AlamofireTraining/Pods-AlamofireTraining-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AlamofireTraining : NSObject
+@end
+@implementation PodsDummy_Pods_AlamofireTraining
+@end
